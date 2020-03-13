7eec653271875167357dec3eb86e0eac9cee1891
PinkDraconian <vanroeyrobbe@hotmail.com>
2020-03-13 12:09:48 +0000

    Update clock.ino

--- clock.ino
+++ clock.ino
@@ -1,23 +1,28 @@
 #include <FastLED.h>
-#define NUM_LEDS 60
-#define DATA_PIN 11
+#define NUM_LEDS 58
+#define DATA_PIN 6
 CRGB leds[NUM_LEDS];
 
 const int TOTAL_SEGMENTS = 4;
-const int[] DISPLAY_NUMBER = {0, 14, 14 * 2 + 2, 14 * 3 + 2};
+const int DISPLAY_SEGMENT[] = {0, 14, 14 * 2 + 2, 14 * 3 + 2};
+const int DISPLAY_NUMBER[][] = {
+  {0, 1, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13} // 0
+};
 
-void setup() { 
+void setup() {
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 }
 
 void loop() {
+  int numberToDisplay = 0;
   for (int i = 0; i < TOTAL_SEGMENTS; i++) {
-  	leds[DISPLAY_NUMBER[i]] = CRGB::Red;
-    FastLED.show()
+    for (int j = 0; j < sizeof DISPLAY_NUMBER[numberToDisplay]; j++) {
+      leds[DISPLAY_SEGMENT[i + j]] = CRGB::Red;
+      FastLED.show();
+    }
   }
   
   // leds[0] = CRGB::Red;
   // FastLED.show(); 
   delay(30);
 }
-
