3c9d364ab143a10921548b46c2b83b9f3d1fbfb7
PinkDraconian <vanroeyrobbe@hotmail.com>
2020-03-13 14:44:10 +0000

    Change

--- clock.ino
+++ clock.ino
@@ -4,6 +4,7 @@
 
 #define NUM_LEDS 58
 #define DATA_PIN 6
+
 CRGB leds[NUM_LEDS];
 DS3231 rtc;
 
@@ -22,6 +23,11 @@ const int DISPLAY_NUMBER[][14] = { // True: Lit, False:  Not lit
   {true, true, true, true, true, true, true, true, true, true, true, true, true, true}, // 8
   {false, false, true, true, true, true, true, true, true, true, true, true, true, true}, // 9
 };
+
+int red = 255;
+int green = 0;
+int blue = 0;
+char currentFade = 'r';
 
 void setup() {
   Serial.begin(9600);
@@ -47,23 +53,39 @@ void loop() {
   int minuteFirstDigit = minute / 10; // Take the first digit
   int minuteSecondDigit = minute % 10; // Take the second digit
 
-  displayNumber(3, hourFirstDigit);
-  displayNumber(2, hourSecondDigit);
-  displayNumber(1, minuteFirstDigit);
-  displayNumber(0, minuteSecondDigit);
-
   leds[14 * 2] = CRGB::Red; // Light the dots
   leds[14 * 2 + 1] = CRGB::Red;
   
-  FastLED.show(); // Show the current LEDs
-  delay(10000); // Wait 10 seconds
-  FastLED.clear(); // Clear the LEDs
+  int totalDelay = 0
+  while (totalDelay < 10000) { // 10 seconds
+    if (currentFade == 'r') { // If red is currently fading
+      red--;
+      green++;
+      currentFade = red <= 0 ? 'g' : 'r'; // Change currentFade if necessary
+    } else if (currentFade == 'g') { // If green is currently fading
+      green--;
+      blue++;
+      currentFade = green <= 0 ? 'b' : 'g'; // Change currentFade if necessary
+    } else { // If blue is currently fading
+      blue--;
+      red++;
+      currentFade = blue <= 0 ? 'r' : 'b'; // Change currentFade if necessary
+    }
+    FastLED.clear(); // Clear the LEDs
+    displayNumber(3, hourFirstDigit);
+    displayNumber(2, hourSecondDigit);
+    displayNumber(1, minuteFirstDigit);
+    displayNumber(0, minuteSecondDigit);
+    FastLED.show(); // Show the current LEDs
+    delay(10);
+    totalDelay += 10;
+  }
 }
 
 void displayNumber(int segment, int number) {
   for (int j = 0; j < LEDS_PER_SEGMENT; j++) { // Loop over each LED of said segment
     if (DISPLAY_NUMBER[number][j]) { // If this LED should be on
-      leds[DISPLAY_SEGMENT[segment] + j] = CRGB::Red; // Turn it on
+      leds[DISPLAY_SEGMENT[segment] + j].setRGB(red, green, blue); // Turn it on
     }
   }
 }
