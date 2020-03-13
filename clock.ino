5d08d0401977a1a715d4ad1f3dcba36574b97e7a
PinkDraconian <vanroeyrobbe@hotmail.com>
2020-03-13 12:19:32 +0000

    Update clock.ino

--- clock.ino
+++ clock.ino
@@ -5,8 +5,8 @@ CRGB leds[NUM_LEDS];
 
 const int TOTAL_SEGMENTS = 4;
 const int DISPLAY_SEGMENT[] = {0, 14, 14 * 2 + 2, 14 * 3 + 2};
-const int DISPLAY_NUMBER[][12] = {
-  {0, 1, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13} // 0
+const int DISPLAY_NUMBER[][6] = {
+  {0, 2, 3, 4, 5, 6} // 0
 };
 
 void setup() {
