9ea451f061bf51b61d6824b960cbeb6f754ba8d7
PinkDraconian <vanroeyrobbe@hotmail.com>
2020-03-13 12:56:18 +0000

    Change

--- clock.ino
+++ clock.ino
@@ -4,9 +4,10 @@
 CRGB leds[NUM_LEDS];
 
 const int TOTAL_SEGMENTS = 4;
+const int LEDS_PER_SEGMENT = 14;
 const int DISPLAY_SEGMENT[] = {0, 14, 14 * 2 + 2, 14 * 3 + 2};
-const int DISPLAY_NUMBER[][12] = {
-  {0, 1, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13} // 0
+const int DISPLAY_NUMBER[][14] = {
+  {true, true, false, false, true, true, true, true, true, true, true, true, true, true} // 0
 };
 
 void setup() {
@@ -19,10 +20,10 @@ void setup() {
 void loop() {
   int numberToDisplay = 0;
   for (int i = 0; i < TOTAL_SEGMENTS; i++) {
-    for (int j = 0; j < sizeof DISPLAY_NUMBER[numberToDisplay]; j++) {
-      Serial.println(DISPLAY_NUMBER[numberToDisplay][j]);
-      Serial.println("Number " + 1 + ": " + (DISPLAY_SEGMENT[i] + DISPLAY_NUMBER[numberToDisplay][j]));
-      leds[DISPLAY_SEGMENT[i] + DISPLAY_NUMBER[numberToDisplay][j]] = CRGB::Red;
+    for (int j = 0; j < LEDS_PER_SEGMENT; j++) {
+      if (DISPLAY_NUMBER[numberToDisplay][j]) {
+        leds[DISPLAY_SEGMENT[i] + j] = CRGB::Red;
+      }
     }
   }
   
